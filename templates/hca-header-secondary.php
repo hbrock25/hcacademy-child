0cf513f74a61ff9d6c35e866d6fd8e33de81e7f2
Hugh Brock <hbrock@gmail.com>
2017-04-08 19:24:14 +0200

    add HC logo image

--- templates/hca-header-secondary.php
+++ templates/hca-header-secondary.php
@@ -14,13 +14,13 @@ $content_2 = avada_secondary_header_content( 'header_right_content' );
     <div class="fusion-row">
         <div class="fusion-alignleft" >
 	    <a href="https://www.harpcolumn.com">
-		<img src="/wp-content/themes/hcacademy-child/images/hc-banner-logo.jpg" />
+		<img class="hca-banner" src="/wp-content/themes/hcacademy-child/images/hc-banner-logo.jpg" />
 	    </a>
 	    <a href="https://harpcolumnmusic.com">
-		<img src="/wp-content/themes/hcacademy-child/images/hcmusic-logo.png" />
+		<img class="hca-banner" src="/wp-content/themes/hcacademy-child/images/hcmusic-logo.png" />
 	    </a>
 	    <a href="/">
-		<img class="selected" src="/wp-content/uploads/2016/06/HCA-logo-web.png" />
+		<img class="hca-banner selected" src="/wp-content/uploads/2016/06/HCA-logo-web.png" />
 	    </a>
 	</div>
         <?php if ( $content_2 ) : ?>
