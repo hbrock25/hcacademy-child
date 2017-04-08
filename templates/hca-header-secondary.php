b5acbb36a2256ed0bc133eba23fa24404d99fe2c
Hugh Brock <hbrock@gmail.com>
2017-04-08 17:59:25 +0200

    Styling changes

--- templates/hca-header-secondary.php
+++ templates/hca-header-secondary.php
@@ -10,15 +10,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 $content_2 = avada_secondary_header_content( 'header_right_content' );
 ?>
 
-<div class="fusion-secondary-header">
-    <div class="fusion-row">
+<div class="fusion-secondary-header" >
+    <div class="fusion-row" style="background-color: #FFF">
         <div class="fusion-alignleft">
-	    <img src="/wp-content/uploads/2016/06/HCA-logo-web.png" height="50"/>
+	    <img src="/wp-content/uploads/2016/06/HCA-logo-web.png" height="30"/>
 	</div>
         <?php if ( $content_2 ) : ?>
             <div class="fusion-alignright"><?php echo $content_2; ?></div>
         <?php endif; ?>
     </div>
+    <div class="fusion-row" style="min-height: 2px"></div>
 </div>
 
 
