9bb53731674b55402396c50ad966dcc7068e23db
Hugh Brock <hbrock@gmail.com>
2017-04-08 18:06:25 +0200

    Styling changes

--- templates/hca-header-secondary.php
+++ templates/hca-header-secondary.php
@@ -10,16 +10,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 $content_2 = avada_secondary_header_content( 'header_right_content' );
 ?>
 
-<div class="fusion-secondary-header" >
-    <div class="fusion-row" style="background-color: #FFF">
+<div class="fusion-secondary-header" style="background-color: #FFF">
+    <div class="fusion-row">
         <div class="fusion-alignleft">
-	    <img src="/wp-content/uploads/2016/06/HCA-logo-web.png" height="30"/>
+	    <a href="https://www.harpcolumn.com">
+		<img style="height: 70" src="/wp-content/uploads/2016/06/HCA-logo-web.png" />
+	    </a>
+	    <a href="https://harpcolumnmusic.com">
+		<img style="height: 70" src="/wp-content/uploads/2016/06/HCA-logo-web.png" />
+	    </a>
+	    <a href="/">
+		<img style="height: 70" src="/wp-content/uploads/2016/06/HCA-logo-web.png" />
+	    </a>
 	</div>
         <?php if ( $content_2 ) : ?>
             <div class="fusion-alignright"><?php echo $content_2; ?></div>
         <?php endif; ?>
     </div>
-    <div class="fusion-row" style="min-height: 2px"></div>
+    <div class="fusion-row" style="min-height: 2px; background-color: #cdb200"></div>
 </div>
 
 
